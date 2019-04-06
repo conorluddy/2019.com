d0dc1bc58907717db4dcdf20e35d80e319264f1a
Conor Luddy <cluddy@gilt.com>
2019-04-06 17:07:04 +0100

    feat: Initial commit. Hello new website...

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <div>
+      <h1 class="title">
+        nuxt-blog-pwa
+      </h1>
+      <blog-post-list />
+    </div>
+  </section>
+</template>
+
+<script>
+import BlogPostList from '~/components/BlogPostList.vue'
+
+export default {
+  components: {
+    BlogPostList
+  }
+}
+</script>
+
+<style />
