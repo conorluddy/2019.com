a22ce47171dd2189779d08859a2220b1674e0831
Conor Luddy <cluddy@gilt.com>
2019-04-24 21:29:06 +0100

    feat: Core CSS up and running

--- pages/blog.vue
+++ pages/blog.vue
@@ -4,13 +4,18 @@
       <h1 class="title">
         nuxt-blog-pwa
       </h1>
+      <blog-post-list />
     </div>
   </section>
 </template>
 
 <script>
+import BlogPostList from '~/components/BlogPostList.vue'
+
 export default {
-  components: {}
+  components: {
+    BlogPostList
+  }
 }
 </script>
 
